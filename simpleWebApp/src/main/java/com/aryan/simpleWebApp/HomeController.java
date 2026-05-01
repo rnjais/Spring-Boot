78f21edf30abed79b9e2fda3c53f996550a4be00
Aryan Jaiswal <your-github-aj80529754@gmail.com>
2026-05-01 12:44:46 +0530

    First commit

---
+++ simpleWebApp/src/main/java/com/aryan/simpleWebApp/HomeController.java
@@ -0,0 +1,25 @@
+package com.aryan.simpleWebApp;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping ("/")
+    public String greet(){
+        return "Hello Good Morning";
+    }
+    @RequestMapping("/to")
+    public String to(){
+        return "Aryan";
+    }
+    @GetMapping("/user")
+    public Map<String, String> getUser() {
+        return Map.of("name", "Aryan", "role", "Developer");
+    }
+}
---
+++ simpleWebApp/src/main/java/com/aryan/simpleWebApp/SimpleWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.aryan.simpleWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebAppApplication {k
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebAppApplication.class, args);
+	}
+
+}
